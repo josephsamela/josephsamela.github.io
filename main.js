7bb2e3178315531eafad3558524bd05a4e51ddb4
Joseph Samela <josephsamela@gmail.com>
2020-11-18 16:40:50 -0500

    update with new photo

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-
 function update() {
-    
+
     // Set background and <p> to random color
     var colors = ["#1BE7FF", "#6EEB83", "#E8AA14", "#FF5714", "083D77", "F95738"];
     var color = colors[Math.floor(Math.random() * colors.length)];
+
     document.body.style.backgroundColor = color;
-    var p = document.getElementsByTagName("p");
-    for (var i = 0; i < p.length; i++) {
-        p[i].style.color = color;
-    }
+    // var p = document.getElementsByTagName("p");
+    // for (var i = 0; i < p.length; i++) {
+    //     p[i].style.color = color;
+    // }
 
     // Change mobile chrome navbar color to compliment
     var allMetaElements = document.getElementsByTagName('meta');
-    for (var i=0; i<allMetaElements.length; i++) {
+    for (var i = 0; i < allMetaElements.length; i++) {
         if (allMetaElements[i].getAttribute("name") == "theme-color") {
             allMetaElements[i].setAttribute('content', color);
             break;
